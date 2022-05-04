352a404cef7e6c89138e4db53e5543bf0af437e4
CAUmang <103166884+CAUmang@users.noreply.github.com>
2022-05-04 23:00:16 +0530

    Uploaded code for Length of all videos in a folder

---
+++ Python File Length code.py
@@ -0,0 +1,20 @@
+import os
+from pymediainfo import MediaInfo
+
+def get_track_len(file_path):
+    media_info = MediaInfo.parse(file_path)
+    for track in media_info.tracks:
+        if track.track_type == "Video":
+            return int(track.duration)
+    return 0
+    
+length_millisec = sum(get_track_len(f) for f in os.listdir(r'C:\Users\ayush\Desktop\dron'))
+length_sec = length_millisec/1000
+length_min = length_sec/60
+
+print(str(length_sec) + ' seconds')
+print(str(length_min) + ' minutes')
+
+
+
+
